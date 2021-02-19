fd185ea121398d8b05000df06ebabebe5122e42a
Jairo <Jairo@LAPTOP-TE9AK6EP>
2021-02-19 16:50:31 -0600

    Primera actividad

---
+++ src/actividad5/pkg1_github_60223/Actividad51_GitHub_60223.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package actividad5.pkg1_github_60223;
+
+/**
+ *
+ * @author Jairo
+ */
+public class Actividad51_GitHub_60223 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String nombre= "Arely Salazar";
+        
+    }
+    
+}
